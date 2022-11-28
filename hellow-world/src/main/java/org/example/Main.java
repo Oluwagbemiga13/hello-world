b9a855ff322cabe023955e8070d58b58bf22dabb
Oluwagbemiga13 <daniel.rakovsky@seznam.cz>
2022-11-28 17:46:49 +0100

    Hello world 1

--- hellow-world/src/main/java/org/example/Main.java
+++ hellow-world/src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Random random = new Random();
+
+        int randomInt = random.nextInt();
+
+        if(randomInt%2 == 0){
+            System.out.println("1");
+        }
+        else {
+            System.out.println("2");
+        }
+
+
     }
 }
